35350d7ea27a2dbb4d831ecc96a2220680e2145d
James Kong <104340270+jameskongchief@users.noreply.github.com>
2023-12-01 21:24:58 +0800

    first commit

--- app/src/main/java/com/example/launchedeffectwithshareflow/MainActivity.kt
+++ app/src/main/java/com/example/launchedeffectwithshareflow/MainActivity.kt
@@ -3,15 +3,21 @@ package com.example.launchedeffectwithshareflow
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.launchedeffectwithshareflow.ui.theme.LaunchedEffectWithShareFlowTheme
@@ -52,7 +58,8 @@ class MyViewModel : ViewModel() {
 fun Greeting(
     viewModel: MyViewModel = viewModel()
 ) {
-    SubView(viewModel.clickEvent, viewModel::onClick)
+        SubView(viewModel.clickEvent, viewModel::onClick)
+
 }
 
 @Composable
@@ -66,7 +73,12 @@ fun SubView(
         }
     }
 
-    Button(onClick = { callBack() }) {
+    Button(
+        onClick = { callBack() },
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+    ) {
         Text(text = "Button")
     }
 }
